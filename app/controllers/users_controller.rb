274d98fbadd52db1d20ec17d082f6323b89b3eba
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-10-24 18:58:35 -0400

    Implemented methods for the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def create
+    @user = User.create!(params.permit(:is_teacher, :first_name, :last_name, :email, :password))
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(params.permit(:is_teacher, :first_name, :last_name, :email, :password))
+    head :no_content
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    head :no_content
+  end
 end
