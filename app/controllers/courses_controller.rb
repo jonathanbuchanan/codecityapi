da148bb708d030784b45aa51f19a6784786855ac
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-10-24 21:10:18 -0400

    Wrote methods for the courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,28 @@
 class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+    render json: @courses
+  end
+
+  def show
+    @course = Course.find(params[:id])
+    render json: @course
+  end
+
+  def create
+    @course = Course.create!(params.permit(:title))
+    render json: @course
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update(params.permit(:title))
+    head :no_content
+  end
+
+  def destroy
+    @course = Course.find(params[:id])
+    @course.delete
+    head :no_content
+  end
 end
