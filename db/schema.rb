d6ed05cb5331771d95aec9d5a4f1204d9fc8b048
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-10-24 21:00:07 -0400

    Generated the model and controller for courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_190506) do
+ActiveRecord::Schema.define(version: 2018_10_25_005743) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "is_teacher"
