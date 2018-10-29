b63f36fe2d4c30a8e4bad83ed3def8156ec1b830
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-10-28 20:53:43 -0400

    Wrote validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { within: 8..50 }
+
   has_and_belongs_to_many :courses
 end
