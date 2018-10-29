fff44237472d7dcade86aa7d418f57f246b9dd4f
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-10-28 20:34:41 -0400

    Using has_secure_password for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_005945) do
+ActiveRecord::Schema.define(version: 2018_10_29_003043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,9 +58,9 @@ ActiveRecord::Schema.define(version: 2018_10_27_005945) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
